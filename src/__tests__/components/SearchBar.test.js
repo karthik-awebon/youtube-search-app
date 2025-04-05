421b481f35d50b02c0e25615d1ac28941cb02eb5
Karthikeyan Balasubramanian <jkarthi.online@gmail.com>
2025-04-05 19:21:20 +0530

    test file reorganized

--- src/__tests__/components/SearchBar.test.js
+++ src/__tests__/components/SearchBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import SearchBar from '../components/SearchBar';
+import SearchBar from '../../components/SearchBar';
 
 describe('SearchBar Component', () => {
   const mockOnSearch = jest.fn();
